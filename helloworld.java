05db9130200fd0f5cf7325b52221e3afb11d2e7f
EdisonGrGr <112044450+EdisonGrGr@users.noreply.github.com>
2022-09-14 18:35:25 -0500

    hola mundo

---
+++ helloworld.java
@@ -0,0 +1,38 @@
+package poo_session1;
+
+import java.util.Scanner;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 23;
+		char letter = 'c';
+		String name = "worker";
+		boolean active = true;
+		if (name.equals("Student")) {
+			System.out.println("the value is correct");
+		} else {
+			System.out.println("the person does not belong to the class");
+		
+System.out.println(name.substring(2));
+		}
+	}
+
+}
+
+	/*/public class PhraseOMatic {
+	    public static void main(String[] args) {
+	        String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B",
+	                "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma",
+	                "critical-path", "dynamic"};
+	        String[] wordListTwo = {"empowered", "sticky", "value-added", "oriented", "centric", 
+	                "distributed", "clustered", "branded", "outside-the-box", "positioned", "networked", 
+	                "f
+}
+ /*/
+int resultado = 0;
+Scanner scanner = new Scanner(System.in);
+System.out.println("Digite un número");
+int numero = scanner.nextInt();
+
